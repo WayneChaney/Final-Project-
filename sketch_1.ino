8e169ccbdcbca41f80c1342b3adb21b4d187f929
WayneChaney <95501786+WayneChaney@users.noreply.github.com>
2022-05-03 20:49:04 -0400

    Add files via upload
    changed variables around make code more readable

--- sketch_1.ino
+++ sketch_1.ino
@@ -5,39 +5,40 @@ SFE_BMP180 bmp180;
 
 void setup() {
   Serial.begin(9600);
-  bool success = bmp180.begin();
+  bool working = bmp180.begin();
 
-  if (success) {
-    Serial.println("BMP180 init success");
+  if (working) {
+    Serial.println("BMP180 code is working ");
   }
 }
 
 void loop() {
-
   char status;
-  double T, P;
+double Temp, Press;
+  
+  
   bool success = false;
 
   status = bmp180.startTemperature();
 
   if (status != 0) {
-    delay(1000);
-    status = bmp180.getTemperature(T);
+    delay(2500);
+    status = bmp180.getTemperature(Temp);
 
     if (status != 0) {
       status = bmp180.startPressure(3);
 
       if (status != 0) {
         delay(status);
-        status = bmp180.getPressure(P, T);
+        status = bmp180.getPressure(Press, Temp);
 
         if (status != 0) {
           Serial.print("Pressure: ");
-          Serial.print(P);
+          Serial.print(Press);
           Serial.println(" hPa");
 
           Serial.print("Temperature: ");
-          Serial.print(T);
+          Serial.print(Temp);
           Serial.println(" C");
         }
       }
